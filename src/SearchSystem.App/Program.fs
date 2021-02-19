64b06a48ab0d8d86c3266c0607133295d4649692
zadykian <zadykian@outlook.com>
2021-02-19 13:39:17 +0300

    *

--- src/SearchSystem.App/Program.fs
+++ src/SearchSystem.App/Program.fs
@@ -1,9 +1,4 @@
-﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
-
-open System
-
-// Define a function to construct a message to print
-let from whom =
+﻿let from whom =
     sprintf "from %s" whom
 
 [<EntryPoint>]
