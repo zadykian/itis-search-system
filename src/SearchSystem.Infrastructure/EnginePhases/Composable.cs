e9f80e93b271bd6594922c1c98ed37256cccf6c0
zadykian <zadykian@outlook.com>
2021-03-13 01:57:32 +0300

    async funcs composition

--- src/SearchSystem.Infrastructure/EnginePhases/Composable.cs
+++ src/SearchSystem.Infrastructure/EnginePhases/Composable.cs
@@ -1,17 +1,22 @@
-// using System;
-// using SearchSystem.Infrastructure.Extensions;
-//
-// namespace SearchSystem.Infrastructure.EnginePhases
-// {
-// 	public static class Composable
-// 	{
-// 		public static Func<TIn, TOut> Compose<TIn, TOut>(Func<TIn, TOut> func) => func;		
-//
-// 		/// <summary>
-// 		/// Create composition of two functions. 
-// 		/// </summary>
-// 		public static Func<TIn, TOut> Compose<TIn, TInterim, TOut>(
-// 			this Func<TIn, TInterim> leftFunc,
-// 			Func<TInterim, TOut> rightFunc) => input => leftFunc(input).To(rightFunc);
-// 	}
-// }
+using System;
+using System.Threading.Tasks;
+
+namespace SearchSystem.Infrastructure.EnginePhases
+{
+	public static class Composable
+	{
+		public static Func<TIn, TOut> Compose<TIn, TOut>(Func<TIn, TOut> func) => func;
+
+		/// <summary>
+		/// Create composition of two functions. 
+		/// </summary>
+		public static Func<TIn, Task<TOut>> Compose<TIn, TInterim, TOut>(
+			this Func<TIn, Task<TInterim>> leftFunc,
+			Func<TInterim, Task<TOut>> rightFunc)
+			=> async input =>
+			{
+				var leftResult = await leftFunc(input);
+				return await rightFunc(leftResult);
+			};
+	}
+}
