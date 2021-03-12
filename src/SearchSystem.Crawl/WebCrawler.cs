ab8d4f5ec79b9309f602b475c7de65439fc4761c
zadykian <zadykian@outlook.com>
2021-03-12 17:56:55 +0300

    *

--- src/SearchSystem.Crawl/WebCrawler.cs
+++ src/SearchSystem.Crawl/WebCrawler.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AngleSharp;
-using SearchSystem.Common;
 using SearchSystem.Common.Configuration;
 using SearchSystem.Common.Extensions;
 using SearchSystem.Crawl.Pages;
