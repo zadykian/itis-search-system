86ec5d32a0e251c15e66e381951d2c131e49684a
zadykian <zadykian@outlook.com>
2021-03-18 18:40:54 +0300

    Sprache

--- src/SearchSystem.BooleanSearch/Parsing/SearchExpressionParser.cs
+++ src/SearchSystem.BooleanSearch/Parsing/SearchExpressionParser.cs
@@ -1,3 +1,5 @@
+using Sprache;
+
 namespace SearchSystem.BooleanSearch.Parsing
 {
 	/// <inheritdoc />
@@ -6,5 +8,14 @@ namespace SearchSystem.BooleanSearch.Parsing
 		/// <inheritdoc />
 		IParseResult ISearchExpressionParser.Parse(string booleanSearchRequest)
 			=> throw new System.NotImplementedException();
+
+		private static class SearchExpressionGrammar
+		{
+			private static Parser<INode.Word> Word =>
+				from openQuote  in Parse.Char(c: '\'').Token()
+				from value      in Parse.CharExcept(c: '\'').Many().Text()
+				from closeQuote in Parse.Char(c: '\'').Token()
+				select new INode.Word(value);
+		}
 	}
 }
