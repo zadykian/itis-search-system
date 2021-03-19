5ae56781fa0d5c1b3881f8f421080f6375a0e98d
zadykian <zadykian@outlook.com>
2021-03-19 15:01:48 +0300

    search expression grammar

--- src/SearchSystem.BooleanSearch/Parsing/SearchExpressionParser.cs
+++ src/SearchSystem.BooleanSearch/Parsing/SearchExpressionParser.cs
@@ -9,13 +9,29 @@ namespace SearchSystem.BooleanSearch.Parsing
 		IParseResult ISearchExpressionParser.Parse(string booleanSearchRequest)
 			=> throw new System.NotImplementedException();
 
+		/// <summary>
+		/// Definition of search expression grammar.
+		/// </summary>
 		private static class SearchExpressionGrammar
 		{
+			private static Parser<INode> ExpressionParser => Parse.Return(new INode.Word(""));
+
+			/// <summary>
+			/// Parser of <see cref="INode.Word"/> sub-expressions. 
+			/// </summary>
 			private static Parser<INode.Word> Word =>
 				from openQuote  in Parse.Char(c: '\'').Token()
 				from value      in Parse.CharExcept(c: '\'').Many().Text()
 				from closeQuote in Parse.Char(c: '\'').Token()
 				select new INode.Word(value);
+
+			/// <summary>
+			/// Parser of <see cref="INode.Not"/> sub-expressions. 
+			/// </summary>
+			private static Parser<INode.Not> Not =>
+				from negationOperator  in Parse.Char(c: '!').Token()
+				from operandExpression in ExpressionParser.Token()
+				select new INode.Not(operandExpression);
 		}
 	}
 }
