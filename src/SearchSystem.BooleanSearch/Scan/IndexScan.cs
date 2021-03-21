729ee39416a6ba38a311b71b54144d6e6ff486c8
zadykian <zadykian@outlook.com>
2021-03-21 20:41:49 +0300

    index scan

--- src/SearchSystem.BooleanSearch/Scan/IndexScan.cs
+++ src/SearchSystem.BooleanSearch/Scan/IndexScan.cs
@@ -13,16 +13,20 @@ namespace SearchSystem.BooleanSearch.Scan
 		IReadOnlyCollection<IDocumentLink> IIndexScan.Execute(IDocumentsIndex index, INode searchExpression)
 			=> RetrieveDocLinks(index, searchExpression);
 
+		/// <summary>
+		/// Retrieve all links from <paramref name="index"/> which
+		/// satisfy expression <paramref name="searchExpression"/>. 
+		/// </summary>
 		private static IImmutableSet<IDocumentLink> RetrieveDocLinks(
 			IDocumentsIndex index,
 			INode searchExpression)
 			=> searchExpression switch
 			{
 				INode.Term term => index.AllWhichContains(term.Value),
-				INode.Not  not  => throw new NotImplementedException(),
-				INode.And  and  => throw new NotImplementedException(),
-				INode.Or   or   => throw new NotImplementedException(),
-				_ => throw new ArgumentOutOfRangeException(nameof(searchExpression))
+				INode.Not  not  => index.AllDocuments().Except(RetrieveDocLinks(index, not.Node)),
+				INode.Or   or   => RetrieveDocLinks(index, or.Left).Union(RetrieveDocLinks(index, or.Right)),
+				INode.And  and  => RetrieveDocLinks(index, and.Left).Intersect(RetrieveDocLinks(index, and.Right)),
+				_ => throw new ArgumentOutOfRangeException(nameof(searchExpression), searchExpression, string.Empty)
 			};
 	}
 }
