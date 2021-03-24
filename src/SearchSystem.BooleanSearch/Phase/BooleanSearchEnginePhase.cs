7e51975ce54efc4b57e4df954a3e5bd86aa2590f
zadykian <zadykian@outlook.com>
2021-03-24 21:38:34 +0300

    boolean search phase

--- src/SearchSystem.BooleanSearch/Phase/BooleanSearchEnginePhase.cs
+++ src/SearchSystem.BooleanSearch/Phase/BooleanSearchEnginePhase.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Threading.Tasks;
+using SearchSystem.BooleanSearch.Parsing;
+using SearchSystem.BooleanSearch.Scan;
 using SearchSystem.BooleanSearch.UserInterface;
 using SearchSystem.Indexing.Index;
 using SearchSystem.Infrastructure.AppEnvironment;
+using SearchSystem.Infrastructure.Extensions;
 using SearchSystem.Infrastructure.SearchEnginePhases;
 using SearchSystem.Normalization.Normalizer;
 
@@ -12,15 +15,21 @@ namespace SearchSystem.BooleanSearch.Phase
 	internal class BooleanSearchEnginePhase : EnginePhaseBase<IDocumentsIndex, Unit>, IBooleanSearchEnginePhase
 	{
 		private readonly IUserInterface userInterface;
+		private readonly ISearchExpressionParser expressionParser;
 		private readonly INormalizer normalizer;
+		private readonly IIndexScan indexScan;
 
 		public BooleanSearchEnginePhase(
 			IUserInterface userInterface,
+			ISearchExpressionParser expressionParser,
 			INormalizer normalizer,
+			IIndexScan indexScan,
 			IAppEnvironment<EnginePhaseBase<IDocumentsIndex, Unit>> appEnvironment) : base(appEnvironment)
 		{
 			this.userInterface = userInterface;
+			this.expressionParser = expressionParser;
 			this.normalizer = normalizer;
+			this.indexScan = indexScan;
 		}
 
 		/// <inheritdoc />
@@ -28,7 +37,27 @@ namespace SearchSystem.BooleanSearch.Phase
 		{
 			userInterface.ShowMessage("enter search expression:");
 			var input = await userInterface.ConsumeInputAsync();
-			
+
+			if (string.Equals(input, "exit", StringComparison.InvariantCultureIgnoreCase))
+			{
+				return Unit.Instance;
+			}
+
+			var parseResult = expressionParser.Parse(input);
+
+			if (parseResult is IParseResult.Failure failure)
+			{
+				userInterface.ShowMessage(failure.ErrorText);
+			}
+
+			var success = (IParseResult.Success) parseResult;
+
+			success
+				.SearchExpression
+				.MapTerms(term => term with { Value = normalizer.Normalize(term.Value)})
+				.To(normalized => indexScan.Execute(inputData, normalized))
+				
+
 			return Unit.Instance;
 		}
 
