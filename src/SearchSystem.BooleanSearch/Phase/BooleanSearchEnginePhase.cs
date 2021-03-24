0b57cdc46502df82f04ed878ff1a4a0194c96b2f
zadykian <zadykian@outlook.com>
2021-03-24 23:32:02 +0300

    boolean search phase

--- src/SearchSystem.BooleanSearch/Phase/BooleanSearchEnginePhase.cs
+++ src/SearchSystem.BooleanSearch/Phase/BooleanSearchEnginePhase.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using SearchSystem.BooleanSearch.Parsing;
 using SearchSystem.BooleanSearch.Scan;
 using SearchSystem.BooleanSearch.UserInterface;
 using SearchSystem.Indexing.Index;
 using SearchSystem.Infrastructure.AppEnvironment;
+using SearchSystem.Infrastructure.Documents.Storage;
 using SearchSystem.Infrastructure.Extensions;
 using SearchSystem.Infrastructure.SearchEnginePhases;
 using SearchSystem.Normalization.Normalizer;
@@ -18,47 +20,53 @@ namespace SearchSystem.BooleanSearch.Phase
 		private readonly ISearchExpressionParser expressionParser;
 		private readonly INormalizer normalizer;
 		private readonly IIndexScan indexScan;
+		private readonly IDocumentStorage documentStorage;
 
 		public BooleanSearchEnginePhase(
 			IUserInterface userInterface,
 			ISearchExpressionParser expressionParser,
 			INormalizer normalizer,
 			IIndexScan indexScan,
+			IDocumentStorage documentStorage,
 			IAppEnvironment<EnginePhaseBase<IDocumentsIndex, Unit>> appEnvironment) : base(appEnvironment)
 		{
 			this.userInterface = userInterface;
 			this.expressionParser = expressionParser;
 			this.normalizer = normalizer;
 			this.indexScan = indexScan;
+			this.documentStorage = documentStorage;
 		}
 
 		/// <inheritdoc />
 		protected override async Task<Unit> ExecuteAnewAsync(IDocumentsIndex inputData)
 		{
 			userInterface.ShowMessage("enter search expression:");
+			var searchRequest = await userInterface.ConsumeInputAsync();
+
+			var resultText = expressionParser.Parse(searchRequest) switch
+			{
+				IParseResult.Success success => success
+					.SearchExpression
+					.MapTerms(term => term with { Value = normalizer.Normalize(term.Value)})
+					.To(normalized =>
+					{
+						var stopwatch = Stopwatch.StartNew();
+						var result = indexScan.Execute(inputData, normalized);
+						return (FoundDocs: result, stopwatch.Elapsed);
+					}) /* todo */,
+
+				IParseResult.Failure failure => failure.ErrorText,
+				_ => throw new ArgumentOutOfRangeException(nameof(IParseResult))
+			};
+
+			userInterface.ShowMessage(resultText);
+
+			userInterface.ShowMessage("exit?");
 			var input = await userInterface.ConsumeInputAsync();
 
-			if (string.Equals(input, "exit", StringComparison.InvariantCultureIgnoreCase))
-			{
-				return Unit.Instance;
-			}
-
-			var parseResult = expressionParser.Parse(input);
-
-			if (parseResult is IParseResult.Failure failure)
-			{
-				userInterface.ShowMessage(failure.ErrorText);
-			}
-
-			var success = (IParseResult.Success) parseResult;
-
-			success
-				.SearchExpression
-				.MapTerms(term => term with { Value = normalizer.Normalize(term.Value)})
-				.To(normalized => indexScan.Execute(inputData, normalized))
-				
-
-			return Unit.Instance;
+			return string.Equals(input, "yes", StringComparison.InvariantCultureIgnoreCase)
+				? Unit.Instance
+				: await ExecuteAnewAsync(inputData);
 		}
 
 		/// <inheritdoc />
