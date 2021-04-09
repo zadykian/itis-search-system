eea96376643e0a43af4abd469bf250f3cddffe16
zadykian <zadykian@outlook.com>
2021-04-09 23:02:19 +0300

    VectorSearchEnginePhase

--- src/SearchSystem.VectorSearch/Phases/VectorSearchEnginePhase.cs
+++ src/SearchSystem.VectorSearch/Phases/VectorSearchEnginePhase.cs
@@ -1,32 +1,70 @@
+using System.Collections.Immutable;
+using System.Linq;
 using System.Threading.Tasks;
 using SearchSystem.Indexing.Index;
 using SearchSystem.Indexing.Phase.External;
+using SearchSystem.Infrastructure.AppEnvironment;
+using SearchSystem.Infrastructure.Extensions;
 using SearchSystem.Infrastructure.SearchEnginePhases;
+using SearchSystem.Normalization.Normalizer;
 using SearchSystem.UserInteraction.Process;
 
 namespace SearchSystem.VectorSearch.Phases
 {
-	/// <inheritdoc/>
-	internal class VectorSearchEnginePhase : ISearchAlgorithmEnginePhase
+	/// <inheritdoc cref="ISearchAlgorithmEnginePhase"/>
+	internal class VectorSearchEnginePhase : TerminatingEnginePhaseBase<ITermsIndex>, ISearchAlgorithmEnginePhase
 	{
 		// todo: move vectorization to separate phase
 		
 		private readonly IStatsCollectionSubphase statsCollectionSubphase;
 		private readonly ISearchProcess searchProcess;
+		private readonly INormalizer normalizer;
 
 		public VectorSearchEnginePhase(
 			IStatsCollectionSubphase statsCollectionSubphase,
-			ISearchProcess searchProcess)
+			ISearchProcess searchProcess,
+			INormalizer normalizer,
+			IAppEnvironment<VectorSearchEnginePhase> appEnvironment) : base(appEnvironment)
 		{
 			this.statsCollectionSubphase = statsCollectionSubphase;
 			this.searchProcess = searchProcess;
+			this.normalizer = normalizer;
 		}
 
 		/// <inheritdoc />
-		async Task<Unit> ISearchEnginePhase<ITermsIndex, Unit>.ExecuteAsync(ITermsIndex inputData)
+		protected override async Task ExecuteAsync(ITermsIndex inputData)
 		{
 			var termEntryStats = await statsCollectionSubphase.ExecuteAsync(inputData);
-			return Unit.Instance;
+
+			var allTermsInCorpus = termEntryStats
+				.Select(entry => entry.Term)
+				.Distinct()
+				.OrderBy(term => term)
+				.ToImmutableArray();
+
+			var vectors = termEntryStats
+				.GroupBy(entry => entry.DocumentLink)
+				.ToDictionary(
+					group => group.Key,
+					group =>
+					{
+						var wordsInDocument = group
+							.DistinctBy(entry => entry.Term)
+							.ToImmutableDictionary(entry => entry.Term, entry => entry.TfIdf);
+
+						return allTermsInCorpus
+							.Select(term => wordsInDocument.TryGetValue(term, out var tfIdf) ? tfIdf : 0d)
+							.ToImmutableArray();
+					});
+
+			await searchProcess.HandleSearchRequests(request =>
+			{
+				var requestWords = request
+					.Split(' ')
+					.Select(token => normalizer.Normalize(token))
+					.ToImmutableArray();
+
+			});
 		}
 	}
 }
